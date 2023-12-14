6f8126d89d8af838556bfd611ef1c93ff0eacb60
Hongssd <p786317875@163.com>
2023-12-14 18:07:02 +0800

    fix codes bug

--- codesdef/forex_code/forex_code.go
+++ codesdef/forex_code/forex_code.go
@@ -2,10 +2,10 @@ package forex_code
 
 const (
 	EURUSD_FX = "EURUSD.FX" //欧元/美元汇率
-	USDJPY_FX = "USDJPX.FX" //美元/日元汇率
+	USDJPY_FX = "USDJPY.FX" //美元/日元汇率
 	GBPUSD_FX = "GBPUSD.FX" //英镑/美元汇率
 	AUDUSD_FX = "AUDUSD.FX" //澳元/美元汇率
 	USDCAD_FX = "USDCAD.FX" //美元/加元汇率
 	USDCHF_FX = "USDCHF.FX" //美元/瑞郎汇率
-	USDCHN_FX = "USDCHN.FX" //美元/人民币汇率
+	USDCNH_FX = "USDCNH.FX" //美元/人民币汇率
 )
